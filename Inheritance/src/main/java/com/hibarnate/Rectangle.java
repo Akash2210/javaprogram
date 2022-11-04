c53537eaf96d1bdeddfd4a24ecfbd1a7c478015b
admin <admin@DESKTOP-9DVGMA0>
2022-11-04 15:36:21 +0530

    Java Hibernate Inheritance Project

---
+++ Inheritance/src/main/java/com/hibarnate/Rectangle.java
@@ -0,0 +1,48 @@
+package com.hibarnate;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+
+@Entity
+@DiscriminatorValue("Rectangle")
+public class Rectangle extends Shape
+{
+		@Column(name="Rec_Length",length=40,nullable=true)
+	    int length ;
+		@Column(name="Rec_breadth",length=30,nullable=true)
+        int breadth ;
+		public Rectangle() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+		public Rectangle(int shapeId) {
+			super(shapeId);
+			// TODO Auto-generated constructor stub
+		}
+		public Rectangle(int shapeId,int length, int breadth) {
+			super();
+			this.length = length;
+			this.breadth = breadth;
+		}
+		public int getLength() {
+			return length;
+		}
+		public void setLength(int length) {
+			this.length = length;
+		}
+		public int getBreadth() {
+			return breadth;
+		}
+		public void setBreadth(int breadth) {
+			this.breadth = breadth;
+		}
+		@Override
+		public String toString() {
+			return "Rectangle [length=" + length + ", breadth=" + breadth + ", getShapeId()=" + getShapeId()
+					+ ", toString()=" + super.toString() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+					+ "]";
+		}
+		
+		
+}
